ccd493ed4e8de309b5f42a2d4493a6cc64bbd3ba
azka1415 <rafifzaydan4@gmail.com>
2022-11-02 14:14:13 +0700

    deleted unused models

--- src/models.py
+++ src/models.py
@@ -13,33 +13,3 @@ class NoteResult(Note):
     ''' note result for the user '''
     created_at: datetime
     owner: str
-
-
-class User(BaseModel):
-    ''' user creation model '''
-    email: EmailStr
-    password: str
-    role: str = 'user'
-
-
-class UserResult(BaseModel):
-    ''' user result for the user '''
-    email: EmailStr
-    created_at: datetime = datetime.now()
-
-
-class Token(BaseModel):
-    ''' token verif model'''
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    ''' check email '''
-    email: Optional[str] = None
-
-
-class EditPassword(BaseModel):
-    ''' edit password model '''
-    old_password: str
-    new_password: str
